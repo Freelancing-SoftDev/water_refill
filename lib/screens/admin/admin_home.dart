bf09f53d8e5a3bac308ca5956f1b6f952310684e
Lans12345 <olanalans12345@gmail.com>
2023-02-19 07:50:38 +0800

    fetched all data in admin

--- lib/screens/admin/admin_home.dart
+++ lib/screens/admin/admin_home.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:water_refill/screens/home_screen.dart';
 import 'package:water_refill/widgets/text_widget.dart';
@@ -22,7 +23,8 @@ class AdminHome extends StatelessWidget {
             ],
             automaticallyImplyLeading: false,
             backgroundColor: Colors.blue,
-            title: TextBold(text: 'HOME', fontSize: 18, color: Colors.white),
+            title:
+                TextBold(text: 'ADMIN HOME', fontSize: 18, color: Colors.white),
             centerTitle: true,
             bottom: const TabBar(tabs: [
               Tab(
@@ -37,79 +39,161 @@ class AdminHome extends StatelessWidget {
             ]),
           ),
           body: TabBarView(children: [
-            StreamBuilder<Object>(
-                stream: null,
-                builder: (context, snapshot) {
-                  return ListView.builder(itemBuilder: ((context, index) {
-                    return Padding(
-                      padding: const EdgeInsets.fromLTRB(20, 5, 20, 5),
-                      child: Card(
-                        elevation: 3,
-                        child: ListTile(
-                          title: TextBold(
-                              text: 'Refilling Station #$index',
-                              fontSize: 16,
-                              color: Colors.black),
-                          subtitle: TextBold(
-                              text: 'Cogon, CDO - 09090104355',
-                              fontSize: 12,
-                              color: Colors.grey),
-                        ),
-                      ),
+            StreamBuilder<QuerySnapshot>(
+                stream: FirebaseFirestore.instance
+                    .collection('Stations')
+                    .snapshots(),
+                builder: (BuildContext context,
+                    AsyncSnapshot<QuerySnapshot> snapshot) {
+                  if (snapshot.hasError) {
+                    print('error');
+
+                    print(snapshot.error);
+
+                    return const Center(child: Text('Error'));
+                  }
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Padding(
+                      padding: EdgeInsets.only(top: 50),
+                      child: Center(
+                          child: CircularProgressIndicator(
+                        color: Colors.black,
+                      )),
                     );
-                  }));
+                  }
+
+                  final data = snapshot.requireData;
+                  return ListView.builder(
+                      itemCount: data.docs.length,
+                      itemBuilder: ((context, index) {
+                        return Padding(
+                          padding: const EdgeInsets.fromLTRB(20, 5, 20, 5),
+                          child: Card(
+                            elevation: 3,
+                            child: ListTile(
+                              title: TextBold(
+                                  text: data.docs[index]['name'],
+                                  fontSize: 16,
+                                  color: Colors.black),
+                              subtitle: TextBold(
+                                  text: '${data.docs[index]['address']}',
+                                  fontSize: 12,
+                                  color: Colors.grey),
+                              trailing: TextBold(
+                                  text:
+                                      '(${data.docs[index]['open']} - ${data.docs[index]['close']})',
+                                  fontSize: 14,
+                                  color: Colors.black),
+                            ),
+                          ),
+                        );
+                      }));
                 }),
-            StreamBuilder<Object>(
-                stream: null,
-                builder: (context, snapshot) {
-                  return ListView.builder(itemBuilder: ((context, index) {
-                    return Padding(
-                      padding: const EdgeInsets.fromLTRB(20, 5, 20, 5),
-                      child: Card(
-                        elevation: 3,
-                        child: ListTile(
-                          title: TextBold(
-                              text: 'Refilling Station #$index',
-                              fontSize: 16,
-                              color: Colors.black),
-                          subtitle: TextBold(
-                              text: 'Cogon, CDO - 09090104355',
-                              fontSize: 12,
-                              color: Colors.grey),
-                          trailing: TextBold(
-                              text: '2 Gallons',
-                              fontSize: 15,
-                              color: Colors.black),
-                        ),
-                      ),
+            StreamBuilder<QuerySnapshot>(
+                stream: FirebaseFirestore.instance
+                    .collection('Orders')
+                    .where('type', isEqualTo: 'Pending')
+                    .snapshots(),
+                builder: (BuildContext context,
+                    AsyncSnapshot<QuerySnapshot> snapshot) {
+                  if (snapshot.hasError) {
+                    print('error');
+
+                    print(snapshot.error);
+
+                    return const Center(child: Text('Error'));
+                  }
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Padding(
+                      padding: EdgeInsets.only(top: 50),
+                      child: Center(
+                          child: CircularProgressIndicator(
+                        color: Colors.black,
+                      )),
                     );
-                  }));
+                  }
+
+                  final data = snapshot.requireData;
+                  return ListView.builder(
+                      itemCount: data.docs.length,
+                      itemBuilder: ((context, index) {
+                        return Padding(
+                          padding: const EdgeInsets.fromLTRB(20, 5, 20, 5),
+                          child: Card(
+                            elevation: 3,
+                            child: ListTile(
+                              title: TextBold(
+                                  text:
+                                      data.docs[index]['gallons'] + ' Gallons',
+                                  fontSize: 16,
+                                  color: Colors.black),
+                              subtitle: TextBold(
+                                  text:
+                                      '${data.docs[index]['name']}- ${data.docs[index]['address']}',
+                                  fontSize: 12,
+                                  color: Colors.grey),
+                              trailing: TextBold(
+                                  text:
+                                      '${int.parse(data.docs[index]['gallons']) * 25} pesos',
+                                  fontSize: 18,
+                                  color: Colors.green),
+                            ),
+                          ),
+                        );
+                      }));
                 }),
-            StreamBuilder<Object>(
-                stream: null,
-                builder: (context, snapshot) {
-                  return ListView.builder(itemBuilder: ((context, index) {
-                    return Padding(
-                      padding: const EdgeInsets.fromLTRB(20, 5, 20, 5),
-                      child: Card(
-                        elevation: 3,
-                        child: ListTile(
-                          subtitle: TextBold(
-                              text: 'Refilling Station #$index',
-                              fontSize: 12,
-                              color: Colors.grey),
-                          title: TextBold(
-                              text: 'John Doe - 2 Gallons',
-                              fontSize: 16,
-                              color: Colors.black),
-                          trailing: TextBold(
-                              text: 'January 25, 2023',
-                              fontSize: 14,
-                              color: Colors.black),
-                        ),
-                      ),
+            StreamBuilder<QuerySnapshot>(
+                stream: FirebaseFirestore.instance
+                    .collection('Orders')
+                    .where('type', isEqualTo: 'Accepted')
+                    .snapshots(),
+                builder: (BuildContext context,
+                    AsyncSnapshot<QuerySnapshot> snapshot) {
+                  if (snapshot.hasError) {
+                    print('error');
+
+                    print(snapshot.error);
+
+                    return const Center(child: Text('Error'));
+                  }
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Padding(
+                      padding: EdgeInsets.only(top: 50),
+                      child: Center(
+                          child: CircularProgressIndicator(
+                        color: Colors.black,
+                      )),
                     );
-                  }));
+                  }
+
+                  final data = snapshot.requireData;
+                  return ListView.builder(
+                      itemCount: data.docs.length,
+                      itemBuilder: ((context, index) {
+                        return Padding(
+                          padding: const EdgeInsets.fromLTRB(20, 5, 20, 5),
+                          child: Card(
+                            elevation: 3,
+                            child: ListTile(
+                              title: TextBold(
+                                  text:
+                                      data.docs[index]['gallons'] + ' Gallons',
+                                  fontSize: 16,
+                                  color: Colors.black),
+                              subtitle: TextBold(
+                                  text:
+                                      '${data.docs[index]['name']}- ${data.docs[index]['stationName']}',
+                                  fontSize: 12,
+                                  color: Colors.grey),
+                              trailing: TextBold(
+                                  text:
+                                      '${int.parse(data.docs[index]['gallons']) * 25} pesos',
+                                  fontSize: 18,
+                                  color: Colors.green),
+                            ),
+                          ),
+                        );
+                      }));
                 }),
           ])),
     );
