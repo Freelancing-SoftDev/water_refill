ce6648f1cdc5ed07d3b1290ce65e8a5e220c64aa
Lans12345 <olanalans12345@gmail.com>
2023-03-01 12:05:46 +0800

    initial config on admin

--- lib/screens/admin/admin_home.dart
+++ lib/screens/admin/admin_home.dart
@@ -9,7 +9,7 @@ class AdminHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      length: 3,
+      length: 2,
       child: Scaffold(
           appBar: AppBar(
             actions: [
@@ -28,9 +28,6 @@ class AdminHome extends StatelessWidget {
             centerTitle: true,
             bottom: const TabBar(tabs: [
               Tab(
-                text: 'Stations',
-              ),
-              Tab(
                 text: 'Orders',
               ),
               Tab(
@@ -39,56 +36,6 @@ class AdminHome extends StatelessWidget {
             ]),
           ),
           body: TabBarView(children: [
-            StreamBuilder<QuerySnapshot>(
-                stream: FirebaseFirestore.instance
-                    .collection('Stations')
-                    .snapshots(),
-                builder: (BuildContext context,
-                    AsyncSnapshot<QuerySnapshot> snapshot) {
-                  if (snapshot.hasError) {
-                    print('error');
-
-                    print(snapshot.error);
-
-                    return const Center(child: Text('Error'));
-                  }
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const Padding(
-                      padding: EdgeInsets.only(top: 50),
-                      child: Center(
-                          child: CircularProgressIndicator(
-                        color: Colors.black,
-                      )),
-                    );
-                  }
-
-                  final data = snapshot.requireData;
-                  return ListView.builder(
-                      itemCount: data.docs.length,
-                      itemBuilder: ((context, index) {
-                        return Padding(
-                          padding: const EdgeInsets.fromLTRB(20, 5, 20, 5),
-                          child: Card(
-                            elevation: 3,
-                            child: ListTile(
-                              title: TextBold(
-                                  text: data.docs[index]['name'],
-                                  fontSize: 16,
-                                  color: Colors.black),
-                              subtitle: TextBold(
-                                  text: '${data.docs[index]['address']}',
-                                  fontSize: 12,
-                                  color: Colors.grey),
-                              trailing: TextBold(
-                                  text:
-                                      '(${data.docs[index]['open']} - ${data.docs[index]['close']})',
-                                  fontSize: 14,
-                                  color: Colors.black),
-                            ),
-                          ),
-                        );
-                      }));
-                }),
             StreamBuilder<QuerySnapshot>(
                 stream: FirebaseFirestore.instance
                     .collection('Orders')
